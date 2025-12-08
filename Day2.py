2fbd320eb6df30d2402db7266b543c3e3ac58f61
Koen Freriks <k.freriks@student.utwente.nl>
2025-12-08 20:37:20 +0100

    day 2 - step 2

--- Day2.py
+++ Day2.py
@@ -1,22 +1,32 @@
-def CheckPair(half1, half2):
-    return half1 == half2
-
-def splitID(id):
-    return id[:(len(id)//2)], id[(len(id)//2):]
+def CheckPair(part1, part2):
+    return part1 == part2
 
 def CheckID(id):
-    first, second = splitID(id)
-    if CheckPair(first, second):
-        return first
-    else:
-        CheckID(first)
+    for splitFactor in range(2,11):
+        # stop after smallest splits is reached
+        if splitFactor > len(id):
+            return False
+        
+        # skip splits of unequal length
+        if (len(id) % splitFactor) != 0:
+            continue
+
+        splitlength = len(id)//splitFactor
+        splits = [id[i*splitlength:(i+1)*splitlength] for i in range(splitFactor)]
+
+        # check if splits are equal
+        if all(x == splits[0] for x in splits):
+            return True
+            
+     # if no repeating patern is found
+    return False
 
 def CheckSequence(sequence):
     start, end = sequence.split('-')
     sum = 0
     
     for id in range(int(start),int(end)+1):
-        if CheckID(str(id)) is not None:
+        if CheckID(str(id)):
             sum += id
     return sum
 
