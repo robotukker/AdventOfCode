6f2780a4ab6336247c190cb2b967f0438e7ecfd3
Koen Freriks <k.freriks@student.utwente.nl>
2025-12-10 09:16:11 +0100

    temp save day 3 - attempt 2

--- Day3.py
+++ Day3.py
@@ -3,7 +3,8 @@ import numpy as np
 def FuseDigits(digitList):
     return "".join(str(int(d)) for d in digitList)
 
-    
+
+        
 
 if __name__ == "__main__":
         
@@ -16,18 +17,38 @@ if __name__ == "__main__":
     for line in lines:
         print(line)
         highDigits = np.zeros(12)
-        i = -1
 
-        for digit in line:
-            i += 1
-            i = 11 if i > 11 else i
-            tempHighDigits = highDigits.copy()
-            tempHighDigits[i] = digit
-            if int(FuseDigits(highDigits)) < int(FuseDigits(tempHighDigits)):
-                highDigits = tempHighDigits.copy()
-                continue
+        skip = 0
+
+        for idx, digit in enumerate(line):
+            k = min(idx, 11)
+            
+            if int(digit) > int(highDigits[k]) and skip < 3:
+
+                if int(highDigits[k]) != 0:
+                    skip += 1
+
+                highDigits[k] = digit
+
+                if int(digit) > int(highDigits[k-1]) and k-1 >= 0 and skip < 3:
+                    skip += 1
+
+                    highDigits[k-1] = digit
+                    highDigits[k] = 0 
+
+                    if int(digit) > int(highDigits[k-2]) and k-2 >= 0 and skip < 3: # dit werkt nog niet met skip
+                        skip += 1
+
+                        highDigits[k-2] = digit
+                        highDigits[k-1] = 0 
+            else:
+                skip += 1
 
         print(highDigits)
+            
+                
+
+
         fusedDigits = FuseDigits(highDigits)
         sum += int(fusedDigits)
 
