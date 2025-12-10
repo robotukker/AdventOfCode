b5222eb8d593489346a66e51ab1b04eba4d19d96
Koen Freriks <k.freriks@student.utwente.nl>
2025-12-10 09:29:16 +0100

    without skip (better)

--- Day3.py
+++ Day3.py
@@ -22,27 +22,21 @@ if __name__ == "__main__":
 
         for idx, digit in enumerate(line):
             k = min(idx, 11)
-            
-            if int(digit) > int(highDigits[k]) and skip < 3:
 
-                if int(highDigits[k]) != 0:
-                    skip += 1
+            if k-2 >= 0 and int(digit) > int(highDigits[k-2]): 
+                highDigits[k-2] = digit
+                highDigits[k-1] = 0 
 
+            elif k-1 >= 0  and int(digit) > int(highDigits[k-1]):
+                highDigits[k-1] = digit
+                highDigits[k] = 0 
+
+            elif skip < 3 and int(digit) > int(highDigits[k]):
                 highDigits[k] = digit
-
-                if int(digit) > int(highDigits[k-1]) and k-1 >= 0 and skip < 3:
-                    skip += 1
-
-                    highDigits[k-1] = digit
-                    highDigits[k] = 0 
-
-                    if int(digit) > int(highDigits[k-2]) and k-2 >= 0 and skip < 3: # dit werkt nog niet met skip
-                        skip += 1
-
-                        highDigits[k-2] = digit
-                        highDigits[k-1] = 0 
+            
             else:
                 skip += 1
+
 
         print(highDigits)
             
