ce490d06e907b1a32585299b30e21ae293152237
Koen Freriks <k.freriks@student.utwente.nl>
2025-12-10 22:57:38 +0100

    day 4 - step 2

--- Day4.py
+++ Day4.py
@@ -13,45 +13,60 @@ if __name__ == "__main__":
     xBorder = matrix.shape[1]
     yBorder = matrix.shape[0]
     accessibleTile = 0
+    removeRolls = []
+    prevRemovedRolls = []
+    running = True
 
     print(matrix)
 
-    for i in range(matrix.shape[0]):
-        for j in range(matrix.shape[1]):
-            adjacentRolls = 0
+    while running:
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                adjacentRolls = 0
 
-            if matrix[i][j] == "@":
-                # vertical and horizontal 
-                if CheckTile(matrix, i+1, j, yBorder,xBorder): # bottom
-                    adjacentRolls += 1
+                if matrix[i][j] == "@":
+                    # vertical and horizontal 
+                    if CheckTile(matrix, i+1, j, yBorder,xBorder): # bottom
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i-1, j, yBorder,xBorder): # top
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i-1, j, yBorder,xBorder): # top
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i, j+1, yBorder,xBorder): # right
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i, j+1, yBorder,xBorder): # right
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i, j-1, yBorder,xBorder): # left
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i, j-1, yBorder,xBorder): # left
+                        adjacentRolls += 1
 
-                # diagonal
-                if CheckTile(matrix, i+1, j+1, yBorder,xBorder): # right-bottom
-                    adjacentRolls += 1
+                    # diagonal
+                    if CheckTile(matrix, i+1, j+1, yBorder,xBorder): # right-bottom
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i-1, j-1, yBorder,xBorder): # left - top
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i-1, j-1, yBorder,xBorder): # left - top
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i-1, j+1, yBorder,xBorder): # right - top
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i-1, j+1, yBorder,xBorder): # right - top
+                        adjacentRolls += 1
 
-                if CheckTile(matrix, i+1, j-1, yBorder,xBorder): # left - bottom 
-                    adjacentRolls += 1
+                    if CheckTile(matrix, i+1, j-1, yBorder,xBorder): # left - bottom 
+                        adjacentRolls += 1
 
-                if adjacentRolls < 4:
-                    accessibleTile += 1
-                    outputMatrix[i][j] = "x"
-  
-    print(outputMatrix)
+                    if adjacentRolls < 4:
+                        accessibleTile += 1
+                        outputMatrix[i][j] = "x"
+                        removeRolls.append([i,j])
+
+        if removeRolls == prevRemovedRolls:
+            running = False
+        prevRemovedRolls = removeRolls.copy()
+
+        # Remove the rolls
+        for i,j in removeRolls:
+            matrix[i,j] = "."
+
+        # print(outputMatrix)
+        # print(matrix)
+
+    print(matrix)
     print(accessibleTile)
-            
 
